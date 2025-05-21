0fdea2945f4654af0c2085e3d3cfe8a49a1ce058
Jenny Ferrufino <jfer.fullstack@gmail.com>
2025-05-20 19:23:06 -0600

    Primer commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/Components/Home';
+import JuegosProvider from './src/Provider/JuegosProvider';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <JuegosProvider>
+          <Home></Home>
+      </JuegosProvider>
+      
     </View>
   );
 }
